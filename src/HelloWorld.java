eab1158fea6658bc46c900ef746deb4d60c8d793
manisha <minaik.31@gmail.com>
2020-01-04 07:24:29 +0530

    main method

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("hi this is manisha");
+
+    }
+
+    public void main(){
+        System.out.println("1stmain");
+    }
+
+    public void main(String x){
+        System.out.println("2stmain");
+    }
+
+}
